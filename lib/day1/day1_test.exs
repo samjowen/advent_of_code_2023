5741a43559624a37ef91b5191386f863960157b7
Samuel Owen <SamHirael@gmail.com>
2023-12-21 23:42:38 +0000

    add failing test "opens file and writes each line into a list of strings"

--- lib/day1/day1_test.exs
+++ lib/day1/day1_test.exs
@@ -5,4 +5,12 @@ defmodule Day1Test do
   test "finds first and last number from array" do
     assert Day1.find_first_and_last_number("1abc2") == {1, 2}
   end
+
+  test "opens file and writes each line to a list of strings" do
+    assert Day1.parse_file("test/day1_test.txt", :string) == [
+             "first line of the file",
+             "second line of the file",
+             "third line of the file"
+           ]
+  end
 end
