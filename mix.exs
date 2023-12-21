792c32163fb0ec65ee999a10f549cf4a308cbae0
Samuel Owen <SamHirael@gmail.com>
2023-12-21 21:51:32 +0000

    Add mix_test_watch dependency to mix.exs

--- mix.exs
+++ mix.exs
@@ -23,7 +23,8 @@ defmodule AdventOfCode2023.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false}
     ]
   end
 end
