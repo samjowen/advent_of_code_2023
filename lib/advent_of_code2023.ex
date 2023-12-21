12cdfab459f9743b06c436847181eb25835db333
Samuel Owen <SamHirael@gmail.com>
2023-12-21 21:32:37 +0000

    Initial commit

---
+++ lib/advent_of_code2023.ex
@@ -0,0 +1,18 @@
+defmodule AdventOfCode2023 do
+  @moduledoc """
+  Documentation for `AdventOfCode2023`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AdventOfCode2023.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/advent_of_code2023_test.exs
@@ -0,0 +1,8 @@
+defmodule AdventOfCode2023Test do
+  use ExUnit.Case
+  doctest AdventOfCode2023
+
+  test "greets the world" do
+    assert AdventOfCode2023.hello() == :world
+  end
+end
