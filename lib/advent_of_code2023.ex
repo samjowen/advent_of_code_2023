ccf67ca51498e10e14c083a22fe863c1022b7348
Samuel Owen <SamHirael@gmail.com>
2023-12-23 16:06:41 +0000

    fix solves the day 1 part I puzzle correctly test

--- lib/advent_of_code2023.ex
+++ lib/advent_of_code2023.ex
@@ -5,9 +5,7 @@ defmodule AdventOfCode2023 do
 
   def day1_solve_part_1(input_file_path) do
     string_list = Day1.parse_file(input_file_path)
-    replaced_number_list = Enum.map(string_list, &Day1.find_first_and_last_number/1)
-    number_list = Enum.map(replaced_number_list, &Day1.combine_first_and_last_number/1)
-
+    number_list = Enum.map(string_list, &Day1.combine_first_and_last_number/1)
     Enum.sum(number_list)
   end
 
