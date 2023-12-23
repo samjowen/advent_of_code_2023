89b90d388a3ad262d393ce720ace8cfef0d7a388
Samuel Owen <SamHirael@gmail.com>
2023-12-23 23:02:16 +0000

    Refactor day1_solve_part_1 and day1_solve_part_2 in AdventOfCode2023

--- lib/advent_of_code2023.ex
+++ lib/advent_of_code2023.ex
@@ -3,43 +3,16 @@ defmodule AdventOfCode2023 do
   Documentation for `AdventOfCode2023`.
   """
 
-  @spec day1_solve_part_1(
-          binary()
-          | maybe_improper_list(
-              binary() | maybe_improper_list(any(), binary() | []) | char(),
-              binary() | []
-            )
-        ) :: number()
   def day1_solve_part_1(input_file_path) do
-    string_list = Day1.parse_file(input_file_path)
-    number_list = Enum.map(string_list, &Day1.combine_first_and_last_number/1)
-    Enum.sum(number_list)
+    Day1.parse_file(input_file_path)
+    |> Enum.map(&Day1.combine_first_and_last_number/1)
+    |> Enum.sum()
   end
 
   def day1_solve_part_2(input_file_path) do
-    # Variables that we need:
-    replacement_map = %{
-      "zero" => "zero0zero",
-      "one" => "one1one",
-      "two" => "two2two",
-      "three" => "three3three",
-      "four" => "four4four",
-      "five" => "five5five",
-      "six" => "six6six",
-      "seven" => "seven7seven",
-      "eight" => "eight8eight",
-      "nine" => "nine9nine"
-    }
-
-    string_list = Day1.parse_file(input_file_path)
-    IO.inspect(string_list)
-
-    replaced_number_list =
-      Enum.map(string_list, &Day1.convert_substrings_to_integer_strings(&1))
-
-    IO.inspect(replaced_number_list)
-    number_list = Enum.map(replaced_number_list, &Day1.combine_first_and_last_number/1)
-    IO.inspect(number_list)
-    Enum.sum(number_list)
+    Day1.parse_file(input_file_path)
+    |> Enum.map(&Day1.convert_substrings_to_integer_strings/1)
+    |> Enum.map(&Day1.combine_first_and_last_number/1)
+    |> Enum.sum()
   end
 end
