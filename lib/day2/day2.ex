2096e9f64717e752f4fe769a565128a5d6d5ab8a
Samuel Owen <SamHirael@gmail.com>
2023-12-24 02:13:03 +0000

    Refactor parse_game_number and add parse_game_sets

--- lib/day2/day2.ex
+++ lib/day2/day2.ex
@@ -6,6 +6,13 @@ defmodule Day2 do
   def parse_game_number(game_string) do
     String.split(game_string, " ", trim: true)
     |> Enum.take(2)
-    |> List.last() |> String.replace(":", "")
+    |> List.last()
+    |> String.replace(":", "")
+  end
+
+  def parse_game_sets(game_string) do
+    Regex.replace(~r/\bGame (\d+):\s/, game_string, "")
+    |> String.split(";")
+    |> Enum.map(&String.trim/1)
   end
 end
