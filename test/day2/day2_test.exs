dd68469923eb9c758b059ffe30a06d6efb147e9c
Samuel Owen <SamHirael@gmail.com>
2023-12-24 00:14:02 +0000

    Add failing tests for "it finds the game number from the game strings"

---
+++ test/day2/day2_test.exs
@@ -0,0 +1,17 @@
+defmodule Day2Test do
+  @moduledoc """
+  This module contains tests for the Day1 module.
+  """
+
+  use ExUnit.Case
+  doctest AdventOfCode2023
+
+  test "it finds the game number from the game strings" do
+    game_string =
+      "Game 1: 7 red, 14 blue; 2 blue, 3 red, 3 green; 4 green, 12 blue, 15 red; 3 green, 12 blue, 3 red; 11 red, 2 green"
+
+    large_game_string = "Game 999: 8 red"
+    assert AdventOfCode2023.get_game_string(game_string) == 1
+    assert AdventOfCode2023.get_game_string(large_game_string) == 999
+  end
+end
