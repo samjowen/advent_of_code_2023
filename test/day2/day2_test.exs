e3c809d112f586549e9e49ebd9bd497c09def74c
Samuel Owen <SamHirael@gmail.com>
2023-12-24 02:03:34 +0000

    Add test for parsing game sets

--- test/day2/day2_test.exs
+++ test/day2/day2_test.exs
@@ -14,4 +14,17 @@ defmodule Day2Test do
     assert Day2.parse_game_number(game_string) == "1"
     assert Day2.parse_game_number(large_game_string) == "999"
   end
+
+  test "it parses the sets from a game string" do
+    game_string =
+      "Game 1: 7 red, 14 blue; 2 blue, 3 red, 3 green; 4 green, 12 blue, 15 red; 3 green, 12 blue, 3 red; 11 red, 2 green"
+
+    assert Day2.parse_game_sets(game_string) == [
+             "7 red, 14 blue",
+             "2 blue, 3 red, 3 green",
+             "4 green, 12 blue, 15 red",
+             "3 green, 12 blue, 3 red",
+             "11 red, 2 green"
+           ]
+  end
 end
